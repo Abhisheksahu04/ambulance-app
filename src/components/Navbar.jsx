fec938f713e99339bdc293726317ff493b224a15
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2025-02-08 22:40:43 +0530

    Add UI components: Label, Progress, Badge, and Tabs; update package dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,18 +70,18 @@ function Navbar(){
           About
         </Link>
         <Link
-          href="#"
+          href="/health-dashboard"
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           prefetch={false}
         >
-          Services
+          Health dashboard
         </Link>
         <Link
           href={"/medical-profile"}
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
           prefetch={false}
         >
-          Contact
+          Medical Profile
         </Link>
       </nav>
     </header>
